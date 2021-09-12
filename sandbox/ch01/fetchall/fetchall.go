8e07dc3f53ce9df8daeed7fce73c80963171b77c
Atsushi KOSHIKIZAWA <lastarrow21@gmail.com>
2021-09-12 23:11:51 +0900

    impl fetchall

---
+++ sandbox/ch01/fetchall/fetchall.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		// begin go-routine
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		// receive from `ch` channel
+		fmt.Println(<-ch)
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	response, err := http.Get(url)
+	if err != nil {
+		// send to channel
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	writtenBytes, err := io.Copy(ioutil.Discard, response.Body)
+	// avoid to leek resource
+	response.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+
+	elapsedTimeSecond := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", elapsedTimeSecond, writtenBytes, url)
+}
