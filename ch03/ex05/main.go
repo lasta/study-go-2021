8b9b15413cd3cf5d32b5c1c1f33f05a52dae48eb
Atsushi KOSHIKIZAWA <lastarrow21@gmail.com>
2021-10-10 02:03:50 +0900

    ch03/ex05

---
+++ ch03/ex05/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(os.Stdout, img)
+	//png.Encode(io.Discard, img)
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			rgba := int(n) << 48 / iterations
+			return color.YCbCr{
+				Y:  uint8(rgba >> 32),
+				Cr: uint8(rgba >> 16),
+				Cb: uint8(rgba),
+			}
+		}
+	}
+	return color.Black
+}
